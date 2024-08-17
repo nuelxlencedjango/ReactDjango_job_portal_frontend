76d0137db761585b01c41cb31fb4b317e5219e3e
nuelxlencedjango <nuelxlence@gmail.com>
2024-08-17 11:25:37 +0100

    accessing artisans

--- src/Components/JobDiv/Jobs.jsx
+++ src/Components/JobDiv/Jobs.jsx
@@ -1,24 +1,30 @@
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { BiTimeFive } from 'react-icons/bi';
 import Search from '../SearchDiv/Search.jsx';
 import { Link } from 'react-router-dom';
 
+
+
+
+
 const Jobs = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem('token');  //localStorage
 
       try {
-        // Update URL to match your backend route
-        const response = await axios.get('https://i-wanwok-backend.up.railway.app/jobs', {
+        const response = await axios.get('https://i-wanwok-backend.up.railway.app', {
           headers: {
             Authorization: `Token ${token}`
+          
           }
+          
         });
-
+      
         setData(response.data);
       } catch (error) {
         console.error("There was an error fetching the services!", error);
@@ -28,14 +34,18 @@ const Jobs = () => {
     fetchData();
   }, []);
 
+
   return (
     <div className="container mx-auto px-4">
+      {/* job search */}
       <Search/>
 
+      {/* job display */}
       <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-10">
-        {data.map(job => (
-          <div key={job.id} className='' data-aos="fade-up">
-            <div className="group group/item singleJob w-full p-4 bg-white rounded-lg hover:bg-[#E5E7EB;] shadow-lg hover:shadow-2xl transition duration-300">
+        {data.map(job => {
+          return (
+            <div className='' data-aos="fade-up">
+            <div key={job.id} className="group group/item singleJob w-full p-4 bg-white rounded-lg hover:bg-[#E5E7EB;] shadow-lg hover:shadow-2xl transition duration-300">
               <div className="flex justify-between items-center mb-2" data-aos="fade-up">
                 <h1 className="text-lg font-semibold text-gray-800 group-hover:text-white">{job.title}</h1>
                 <span className="flex items-center text-gray-400">
@@ -55,15 +65,23 @@ const Jobs = () => {
                 to={`/artisans/artisans-by-service/${encodeURIComponent(job.title)}`} 
                 className="mt-4 border-2 border-green-500 rounded-lg py-2 px-4 w-full text-center text-gray-800 font-semibold hover:bg-green-500 hover:text-white transition duration-300 block"
               >
-                See Workers
+              See Workers
               </Link>
 
             </div>
-          </div>
-        ))}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
+
+
 }
 
 export default Jobs;
+
+
+
+
+
