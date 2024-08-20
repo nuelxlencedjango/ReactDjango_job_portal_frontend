6e41139d8bd41d9e2f6433e836fc44ea2e8415af
nuelxlencedjango <nuelxlence@gmail.com>
2024-08-20 03:48:01 +0100

    order request

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ function RegisterAndLogout(){
 const App = () => {
 
   useEffect(()=>{
-    AOS.init({duration:500}); 
+    AOS.init({duration:1000}); 
 }, []);
 
   return (
--- src/Components/Workers/ArtisanLists.jsx
+++ src/Components/Workers/ArtisanLists.jsx
@@ -89,7 +89,7 @@ const Artisans = () => {
 
   return (
     <div className="container mx-auto px-4 mt-32" data-aos="fade-up">
-      <h1 className="text-2xl font-semibold mb-4 artisanlist-heading">Available {service_title} for your service</h1>
+      <h1 className="text-2xl font-semibold mb-4 artisanlist-heading display-center">Available {service_title} for your service</h1>
       <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 py-10">
         {artisans.map(artisan => (
           <div
