92a3264231d1ef4cddf51ba2977579300420a953
nuelxlencedjango <nuelxlence@gmail.com>
2024-11-24 20:44:00 +0000

    check artisan in the cart

--- src/Components/CartContent/Cart.jsx
+++ src/Components/CartContent/Cart.jsx
@@ -98,7 +98,7 @@ const Cart = () => {
                     className="w-16 h-16 rounded-full object-cover"
                   />
                 ) : (
-                  <div className="w-15 h-15 rounded-full bg-gray-300"></div>
+                  <div className="w-16 h-16 rounded-full bg-gray-300"></div>
                 )}
                 <p className="text-center sm:text-left text-lg font-medium mt-1">
                   {item.artisan.first_name} {item.artisan.last_name}
