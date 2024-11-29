d487f16e6a0fcccc85fe5cbfe761571e9734dc3d
nuelxlencedjango <nuelxlence@gmail.com>
2024-11-29 11:44:11 +0000

    check artisan in the cart

--- src/Components/CartContent/Cart.jsx
+++ src/Components/CartContent/Cart.jsx
@@ -129,8 +129,8 @@ const Cart = () => {
 
               {/* Add Button */}
               <Link
-                to={`/artisans/artisans-by-service/${encodeURIComponent(item.job?.title)}`}
-                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-black-600 color-white transition-all duration-300 transform hover:scale-110"
+                to={`/artisans/artisans-by-service/${encodeURIComponent(item.title)}`}
+                className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-black-600 color-white transition-all duration-300 transform hover:scale-110"
               >
                 Add
               </Link>
