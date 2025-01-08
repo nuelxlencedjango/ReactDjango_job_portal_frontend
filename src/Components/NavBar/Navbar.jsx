b02e897177c40385ac5d81ea72ee9291c467d713
Sammy <badejosamuel9@gmail.com>
2025-01-08 16:21:21 +0100

    i-wan-wok

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
                         
                         {/* logo image */}
                         <Link to={'/'}><img src={logo} alt="Logo" className="w-14 h-14 mr-2 rounded-full" /></Link>
-                        <h1 className='logo text-[25px] text-green-500'><strong><Link to={'/'}>I-won-wok</Link></strong></h1>
+                        <h1 className='logo text-[25px] text-green-500'><strong><Link to={'/'}>I-wan-wok</Link></strong></h1>
                     </div>
                     <div className="hidden md:flex gap-8">
                         <li className='menuList text-[#6f6f6f] hover:text-blue-600'><Link to={'/'}>Jobs</Link></li>
