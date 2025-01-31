64ca1932d0267379dabc252217b7fb5720dbb03e
Sammy <badejosamuel9@gmail.com>
2025-01-31 15:01:33 +0100

    still working on

--- src/Components/Dashboard/ArtisanDashboard.jsx
+++ src/Components/Dashboard/ArtisanDashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Sidebar from './Sidebar'
 
 
 
@@ -8,9 +9,9 @@ const Values=()=> {
   return (
     <div className='mb-[4rem] mt-[6rem]'>
       <h1 className='text-textColor text-[22px] py-[2rem] pb-[3rem] font-bold block'>
-        404
+  <Sidebar />
       </h1>
-      <h4>Artisan's Dashboard</h4>
+      {/* <h4>Artisan's Dashboard</h4> */}
      
     </div>
   )
