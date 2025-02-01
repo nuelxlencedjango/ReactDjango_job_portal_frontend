6ccd2c5f8105a58dd8f2ab51bc9b8ce89681c397
nuelxlencedjango <nuelxlence@gmail.com>
2025-02-01 16:44:39 +0000

    managers'dash

--- src/Components/Workers/ArtisanDashboard.jsx
+++ src/Components/Workers/ArtisanDashboard.jsx
@@ -1,12 +1,19 @@
 // src/pages/ManagerDashboard.jsx
 import React from 'react';
+import { useNavigate, Link } from "react-router-dom";
 
 const ManagerDashboard = () => {
   return (
     <div className="min-h-screen bg-gray-100 p-8">
       <h1 className="text-2xl font-bold text-gray-800">Artisan Dashboard</h1>
       <p className="text-gray-600">Welcome to the Artisan's page!</p>
+
+       <Link to={`/api/artisans-by-se/$`}
+            className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition-all duration-300 flex items-center gap-2">
+            <FaPlus className="text-sm" /> Add Finger Print
+        </Link>
     </div>
+    
   );
 };
 
